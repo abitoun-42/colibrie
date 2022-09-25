b4949aa70d42e63dc2e22cabb106ebe223b51bb1
alexandre bitoun <alexandre@MacBook-Pro-de-alexandre-2.local>
2022-09-25 21:43:02 +0200

    add typing and docstring for utils.py

--- colibrie/utils.py
+++ colibrie/utils.py
@@ -1,10 +1,20 @@
+from typing import Union
+
 import numpy as np
 
 from colibrie.geometry import Rect
 from colibrie.geometry_operation import is_rectangle_overlap, is_rectangle_contained
 
 
-def extract_text_from_spans(spans):
+def extract_text_from_spans(spans: dict) -> str:
+    """
+    This function will extract text information from spans
+
+    :param spans: list of dict containing all information about the text of a cell
+                  like coordinate, orientation, fonts, size
+                  refer to https://pymupdf.readthedocs.io/en/latest/textpage.html#structure-of-dictionary-outputs
+    :return: Text of a cell as a string
+    """
     text = ""
     for span in spans:
         text += (
@@ -14,7 +24,16 @@ def extract_text_from_spans(spans):
     return text
 
 
-def get_text_in_cell(rect, text):
+def get_text_in_cell(rect: Rect, text: list[dict]) -> list[dict]:
+    """
+    This function select all span of text that are contained into a cell coordinate from a list of text spans
+
+    :param rect: a rect clip where we search for the text
+    :param text: a list of dicts extracted from page
+                 refer to https://pymupdf.readthedocs.io/en/latest/textpage.html#TextPage.extractDICT
+                 for more information
+    :return: list of dict where each one represent a text span
+    """
     text_in_cell = []
 
     # Give a 1 px text tolerance around the rect
@@ -48,7 +67,16 @@ def get_text_in_cell(rect, text):
     return text_in_cell
 
 
-def closest_value(input_list, input_value):
+def closest_value(
+    input_list: list[Union[float, int]], input_value: Union[float, int]
+) -> Union[float, int]:
+    """
+    This function return the closest value to input_value from input_list
+
+    :param input_list: The list of number you want to find the closest value from
+    :param input_value: The number you want to find the closest value
+    :return: Closest value from input_list to input_value
+    """
     arr = np.asarray(input_list)
 
     i = (np.abs(arr - input_value)).argmin()
